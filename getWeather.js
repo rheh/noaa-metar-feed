286aaca71640d3dc2650517c483b363a9f1b73d0
Ray Hammond <ray.hammond77@gmail.com>
2015-02-27 21:15:51 +0000

    Added feature to save raw textual METAR to data/ dir

--- getWeather.js
+++ getWeather.js
@@ -3,6 +3,9 @@ var fs = require('fs');
 var NOAALatestFileParser = require('./lib/NOAALatestFileParser');
 var NOAAFile = require('./lib/NOAAFile');
 var FTPHelper = require('./lib/FTPHelper');
+var MetarSaver = require('./lib/MetarSaver');
+var ICAOExtractor = require('./lib/ICAOExtractor');
+
 var ftpHelper = new FTPHelper('tgftp.nws.noaa.gov');
 
 var parseLatestFile = function (filename) {
@@ -11,8 +14,28 @@ var parseLatestFile = function (filename) {
 };
 
 var parseLatestWeatherFile = function (latestWeatherFile) {
+
     var onComplete = function (metars) {
-        console.log(metars);
+
+        var metarSaver = new MetarSaver();
+        var extractor = new ICAOExtractor();
+
+        metars.forEach(function (currentValue, index, array) {
+            
+            try {
+
+                extractor.setMetar(currentValue);
+                var ICAO = extractor.extract();
+
+                metarSaver.setICAO(ICAO);
+                metarSaver.setData(currentValue);
+                metarSaver.save();
+
+            } catch (e) {
+                console.log("Skipping: " + e);
+            }
+
+        });
 
         process.exit(1);
     };
@@ -25,12 +48,12 @@ var parseLatestWeatherFile = function (latestWeatherFile) {
 
 var getLatestWeatherFile = function (latestFile) {
     console.log("Latest weather file", latestFile);
-    ftpHelper.get(lsRemotePath + latestFile, latestFile, parseLatestWeatherFile);
+    ftpHelper.get(lsRemotePath + latestFile, './downloads/' + latestFile, parseLatestWeatherFile);
 };
 
 var lsRemotePath = '/SL.us008001/DF.an/DC.sflnd/DS.metar/';
 var lsRemoteFileName = lsRemotePath + 'ls-lt';
-var lsLocalFileName = 'ls-lt.txt';
+var lsLocalFileName = './downloads/ls-lt.txt';
 
 try {
     ftpHelper.get(lsRemoteFileName, lsLocalFileName, parseLatestFile);
